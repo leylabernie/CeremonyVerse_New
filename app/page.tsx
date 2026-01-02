cb632b3de545b6d405d4388724913acfa4912a89
leylabernie <bhamini@verizon.net>
2026-01-02 13:03:08 -0500

    Import and render HomeWhyMexico after HomeHero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { HomeHero } from "@/components/home/HomeHero";
+import { HomeWhyMexico } from "@/components/home/HomeWhyMexico";
 
 export default function HomePage() {
   return (
     <>
       <HomeHero />
+      <HomeWhyMexico />
     </>
   );
 }
