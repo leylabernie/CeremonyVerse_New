1640fcb0ce6331949a7bdd8489c059130767500b
leylabernie <bhamini@verizon.net>
2026-03-12 13:22:07 -0400

    Adjust image height for responsive design

--- components/cta/EtsyOutfitCard.tsx
+++ components/cta/EtsyOutfitCard.tsx
@@ -13,7 +13,7 @@ export default function EtsyOutfitCard({ title, image, link }: Props) {
         <img
   src={image}
   alt={title}
-  className="w-full h-[520px] object-contain"
+  className="w-full object-contain md:h-[520px]"
 />
       </Link>
 
